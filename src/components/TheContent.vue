f3307eb8e43017f8fc748e4051c4b4a80a64e8c6
Aleksandr Nesterov <RuAleksandr07@gmail.com>
2021-04-24 02:21:17 +0500

    added products list

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1 class="title">Интернет-магазин</h1>
+  <v-container>
+    <h1 class="title">Интернет-магазин</h1>
+    <v-row>
+      <product-item v-for="product in fetchProducts" :key="product.id" :product="product"/>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
+import ProductItem from './ProductItem.vue'
+import apiService from '../api/apiService'
 
+export default {
+  components: { ProductItem },
+  computed: {
+    fetchProducts() {
+      return apiService.fetchProducts();
+    }
+  },
+  mounted() {
+    console.log(apiService.fetchProducts())
+  }
 }
 </script>
 
